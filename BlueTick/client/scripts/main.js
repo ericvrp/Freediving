cb7936ba1baf727d3fa5096ecc2ccc2d76ec2053
Eric van Riet Paap <ericvrp@gmail.com>
2015-07-13 20:31:44 +0200

    Some global template helper. Some refactoring. Use of a basic Bootstrap btn class.

--- BlueTick/client/scripts/main.js
+++ BlueTick/client/scripts/main.js
@@ -1,19 +1,1 @@
-// counter starts at 0
 Session.setDefault('counter', 0);
-
-Template.home.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
-
-Template.home.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
-});
-
-Router.route('/', function () {
-  this.render('home');
-});
---
+++ BlueTick/client/scripts/templates.js
@@ -0,0 +1,18 @@
+Template.home.helpers({
+  counter: function () {
+    return Session.get('counter');
+  }
+});
+
+Template.home.events({
+  'click button': function () {
+    // increment the counter when button is clicked
+    Session.set('counter', Session.get('counter') + 1);
+  }
+});
+
+//Global template helpers
+Template.registerHelper('randColor', function() {
+  choices = ['#bada55','#B43831', '#783BA3', '#00AB1B', '#143275', '#FFA700'];
+  return Random.choice(choices);
+});
